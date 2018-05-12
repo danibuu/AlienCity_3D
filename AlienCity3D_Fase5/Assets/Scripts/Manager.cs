e9687a9304e7eb27455af8eb578dd9d12991e318
Daniel <danielkaiss@hotmail.com>
2018-05-12 01:30:12 -0300

    Alterando Porta

--- AlienCity3D_Fase5/Assets/Scripts/Manager.cs
+++ AlienCity3D_Fase5/Assets/Scripts/Manager.cs
@@ -5,23 +5,56 @@ using UnityEngine;
 public class Manager : MonoBehaviour {
     private GameObject[] key;
     public Animator porta;
+    public bool hasBlue = false;
+    public bool hasGreen = false;
+    public bool canDoAction = false;
+    public GameObject keylogger;
+    private insertKeys inserirChaves;
 	// Use this for initialization
 	void Start () {
         key = GameObject.FindGameObjectsWithTag("key");
+
         //Debug.Log(key.Length);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(VarTemp.holdKeys == key.Length)
+        inserirChaves = keylogger.GetComponent<insertKeys>();
+
+		if (Input.GetKeyDown(KeyCode.R))
         {
-            VarTemp.holdKeys = 0;
-            DoorOpen();
+            if (canDoAction)
+            {
+                DoAction();
+            }
         }
 	}
 
     void DoorOpen()
     {
+        keylogger.GetComponent<Collider>().enabled = false;
         porta.SetTrigger("open");
+    }
+
+    void DoAction()
+    {
+        //Se tiver a chave verde, ativa a imagem dela quando realizar a acao.
+        if (hasGreen)
+        {
+            inserirChaves.greenKeyInsert.SetActive(true);
+            hasGreen = false;
+        }
+        //O mesmo para a azul.
+        if (hasBlue)
+        {
+            inserirChaves.blueKeyInsert.SetActive(true);
+            hasBlue = false;
+        }
+        //Se tiver as duas, abre a porta ao inserir as chaves.
+        if (VarTemp.holdKeys == key.Length)
+        {
+            VarTemp.holdKeys = 0;
+            DoorOpen();
+        }
     }
 }
--- AlienCity3D_Fase5/Assets/Scripts/getKey.cs
+++ AlienCity3D_Fase5/Assets/Scripts/getKey.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class getKey : MonoBehaviour {
+    private GameObject Manager;
+    private Manager GM;
+
+    private void Start()
+    {
+        Manager = GameObject.FindGameObjectWithTag("Manager");
+        GM = Manager.GetComponent<Manager>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (gameObject.name == "key01") //verde
+            {
+                GM.hasGreen = true;
+            }
+            if (gameObject.name == "key02") //azul
+            {
+                GM.hasBlue = true;
+            }
             VarTemp.holdKeys++;
             //Debug.Log(VarTemp.holdKeys);
             Destroy(gameObject);
         }
+
+        
     }
 }
